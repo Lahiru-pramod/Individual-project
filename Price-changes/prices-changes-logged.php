70adcde3c560b7b3814e3641b257c38a63fc0c4e
lahiru Pramod <lahirupramod41@gmail.com>
2021-09-18 19:37:50 +0530

    2021.09.18 7.37pm Signed-off-by: lahiru Pramod <lahirupramod41@gmail.com>

--- Price-changes/prices-changes-logged.php
+++ Price-changes/prices-changes-logged.php
@@ -1,3 +1,82 @@
+<?php session_start(); ?>
+<?php require_once('../DBconnection/connection.php'); ?>
+<?php require_once('../DBconnection/functions.php'); ?>
+
+<?php
+
+if(isset($_POST['adult'])){
+
+    $adult=  mysqli_real_escape_string($connection, $_POST['adultprice']);
+
+    $query= "UPDATE prices SET Adult = '{$adult}' WHERE ID = 1";
+
+    $result = mysqli_query($connection, $query);
+    if ($result){
+     // if successful
+    echo'<script> alert("Do you want change Adult price?");
+            </script>';
+
+    }else{
+
+       echo'<script> alert("Process is Falied, Please try again")</script>';
+
+
+    };
+
+
+};
+
+if(isset($_POST['child'])){
+
+    $child=  mysqli_real_escape_string($connection, $_POST['childprice']);
+
+    $query= "UPDATE prices SET child = '{$child}' WHERE ID = 1";
+
+    $result = mysqli_query($connection, $query);
+    if ($result){
+     // if successful
+    echo'<script> alert("Do you want change Child price?");
+            </script>';
+
+    }else{
+
+       echo'<script> alert("Process is Falied, Please try again")</script>';
+
+
+    };
+
+
+};
+
+if(isset($_POST['meal'])){
+
+    
+
+    $meal=  mysqli_real_escape_string($connection, $_POST['mealprice']);
+
+    $query= "UPDATE prices SET meal = '{$meal}' WHERE ID = 1";
+    
+
+    $result = mysqli_query($connection, $query);
+    if ($result){
+     // if successful
+    echo'<script> confirm("Do you want change meal price?");
+            </script>';
+
+
+    }else{
+
+       echo'<script> alert("Process is Falied, Please try again")</script>';
+
+
+    };
+
+
+};
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,31 +108,124 @@
     <div class="row d-flex justify-content-around" id="row2">
 
         <div class="row pt-5 " id="row2-row1">
+
             <div class="col" id="boxes"  >
-                <h3>Normal Adult</h3>
-                <input type="text" name="" id="fixinput" value="500.00 / 1 per"><br>
-                <input type="text" placeholder="Change price.."><br>
-                <input type="button" value="Change" id="change" ><br>
+                <h3>Normal Adult</h3> 
+
+                <?php
+                
+                $connection = mysqli_connect('localhost', 'root', '', 'captaincruise');
+                $sql = "SELECT Adult FROM prices order by ID desc";
+                $result = mysqli_query($connection , $sql);
+                while($row = mysqli_fetch_array($result)){
+
+                echo"<h3 style='color:red;'>".$row['Adult']." </h3>";
+
+                };
+
+                
+                ?>
+                <form action="prices-changes-logged.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="adultprice" placeholder="Change price.."><br>
+                <input type="submit" value="Change" id="change" name="adult" ><br>
+                </form>
 
             </div>
             <div class="col" id="boxes" >
                 <h3>Normal Child</h3>
-                <input type="text" name="" id="fixinput" value="250.00 / 1 per"><br>
-                <input type="text" placeholder="Change price.."><br>
-                <input type="button" value="Change" id="change" ><br>
+
+                <?php
+                
+                $connection = mysqli_connect('localhost', 'root', '', 'captaincruise');
+                $sql = "SELECT child FROM prices order by ID desc";
+                $result = mysqli_query($connection , $sql);
+                while($row = mysqli_fetch_array($result)){
+
+                echo"<h3 style='color:red;'>".$row['child']." </h3>";
+
+                };
+                ?>
+
+                <form action="prices-changes-logged.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="childprice" placeholder="Change price.."><br>
+                <input type="submit" value="Change" id="change" name="child" ><br>
+                </form>
 
             </div>
             <div class="col" id="boxes" >
                 <h3>Meal price</h3>
-                <input type="text" name="" id="fixinput" value="300.00 / 1 per"><br>
-                <input type="text" placeholder="Change price.."><br>
-                <input type="button" value="Change" id="change" ><br>
+                
+                <?php
+                
+                $connection = mysqli_connect('localhost', 'root', '', 'captaincruise');
+                $sql = "SELECT meal FROM prices order by ID desc";
+                $result = mysqli_query($connection , $sql);
+                while($row = mysqli_fetch_array($result)){
+
+                echo"<h3 style='color:red;'>".$row['meal']." </h3>";
+
+                };
+                ?>
+                <form action="prices-changes-logged.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="mealprice" placeholder="Change price.."><br>
+                <input type="submit" value="Change" id="change" name="meal" ><br>
+                </form>
 
             </div>
         </div>
 
 
     </div>
+
+    <!-- Prices close -->
+
+    <!-- Special Packages -->
+
+    <div class="row text-center mt-5 bg-dark" id="row4">
+        <h3 style="color: white;">- Special Package Prices - </h3>
+    </div>
+
+
+
+    <div class="row mt-4 d-flex justify-content-around" id="row5">
+       <div class="row text-center" id="row5-1">
+           <div class="col" id="boxes">
+               <div class="row" id="box-image">
+
+               <h2>Lahiru</h2>
+               <img src="./images/6308.jpg" alt="">
+
+               </div>
+           </div>
+
+           <div class="col" id="boxes">
+               <h2>Lahiru</h2>
+           </div>
+
+           <div class="col" id="boxes">
+               <h2>Lahiru</h2>
+           </div>
+
+           <div class="col" id="boxes">
+               <h2>Lahiru</h2>
+           </div>
+
+
+       </div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
     </div>
 </div>
 
